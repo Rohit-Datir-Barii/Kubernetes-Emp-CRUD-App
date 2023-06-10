1e47a87c9acc03fb7f211b396fcc6456051e6a0d
Rohit-Datir-Barii <rohitdatir143@gmail.com>
2023-06-10 22:50:26 +0530

    changed properties file

--- src/main/java/com/rd/controller/EmployeeController.java
+++ src/main/java/com/rd/controller/EmployeeController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rd.entity.Employee;
 import com.rd.service.EmployeeService;
 
 @Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
 	@Autowired
